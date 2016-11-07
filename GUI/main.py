65623937eee5497ced253328b5b258cee8a7b209
Tmn07 <519043202@qq.com>
2016-11-07 19:40:56 +0800

    gui of k

---
+++ GUI/main.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+import  wx
+
+import Panel1
+#----------------------------------------------------------------------------
+
+TitleTexts = [ u"K操作",
+              u"Fasta算法",
+              u"编辑距离",
+              u"NW&SW算法",
+              u"ID3决策树"
+              ]
+          
+
+class TestCB(wx.Choicebook):
+    def __init__(self, parent):
+        wx.Choicebook.__init__(self, parent, wx.ID_ANY)
+
+        # Now make a bunch of panels for the choice book
+        count = 1
+        for txt in TitleTexts:
+            if count == 1:
+                # win = wx.Panel(self)
+                win = Panel1.Panel1(self)
+                # st = wx.StaticText(win, -1,
+                #           "wx.Choicebook is yet another way to switch between 'page' windows",
+                #           (10, 10))
+            else:
+                win = wx.Panel(self)
+                st = wx.StaticText(win, -1, "Page: %d" % count, (10,10))
+            count += 1
+            
+            self.AddPage(win, txt)
+
+        # self.Bind(wx.EVT_CHOICEBOOK_PAGE_CHANGED, self.OnPageChanged)
+        # self.Bind(wx.EVT_CHOICEBOOK_PAGE_CHANGING, self.OnPageChanging)
+
+
+    def OnPageChanged(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        event.Skip()
+
+    def OnPageChanging(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        event.Skip()
+
+
+########################################################################
+class DemoFrame(wx.Frame):
+    """
+    Frame that holds all other widgets
+    """
+
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""        
+        wx.Frame.__init__(self, None, wx.ID_ANY, 
+                          u" 生物信息学I 实验",
+                          size=(800,600))
+
+        panel = wx.Panel(self)
+        
+        notebook = TestCB(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(notebook, 1, wx.ALL|wx.EXPAND, 5)
+        panel.SetSizer(sizer)
+        self.Layout()
+        
+        self.Show()
+        
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    app = wx.PySimpleApp()
+    frame = DemoFrame()
+    app.MainLoop()
+    
